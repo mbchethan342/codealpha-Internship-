600ba868daf6766479f1ce9759c082abc1ef6f88
mbchethan342 <154079778+mbchethan342@users.noreply.github.com>
2023-12-20 09:51:15 -0800

    Update 2script.js

--- 2script.js
+++ 2script.js
@@ -1,88 +1,22 @@
-let disBlock = document.getElementById('disBlock');
-let age = document.getElementById('age');
-let time = document.getElementById('time');
-let dobDate = document.getElementById('dobDate');
-let calcBtn = document.getElementById('calcBtn');
-let resetBtn = document.getElementById('resetBtn');
+function calculateAge() {
+    // Get the input value (birthdate)
+    var birthdate = document.getElementById('birthDate').value;
 
-disBlock.style.display = 'none';
+    // Check if a birthdate is selected
+    if (birthdate !== '') {
+        // Calculate the age
+        var today = new Date();
+        var birthdateDate = new Date(birthdate);
+        var age = today.getFullYear() - birthdateDate.getFullYear();
 
-let dob = new Date(), today = new Date();
-let calTime; // Declare calTime globally
-
-function samay() {
-    let d = new Date();
-    time.innerHTML = d.getHours() + "Hours," + d.getMinutes() + "Minutes," + d.getSeconds() + "Seconds Old";
-}
-
-function calculate() { // Corrected function name
-    disBlock.style.display = 'block';
-    let dobVal = dobDate.value;
-
-    dob = new Date(dobDate.value); // Reassign the global variable dob
-    console.log(dob);
-
-    let year, month, day;
-
-    day = (function() {
-        if (today.getDate() > dob.getDate()) {
-            return today.getDate() - dob.getDate() - 1;
-        } else if (today.getDate() === dob.getDate()) {
-            return today.getDate() - dob.getDate();
-        } else {
-            let calDate = new Date(dob.getFullYear(), dob.getMonth() + 1, 0);
-            return (today.getDate() + calDate.getDate()) - dob.getDate() - 1;
+        // Check if birthday has occurred this year
+        if (today.getMonth() < birthdateDate.getMonth() || (today.getMonth() === birthdateDate.getMonth() && today.getDate() < birthdateDate.getDate())) {
+            age--;
         }
-    })();
 
-    month = (function() {
-        if (today.getMonth() >= dob.getMonth()) {
-            if (today.getDate() >= dob.getDate()) {
-                return today.getMonth() - dob.getMonth();
-            } else {
-                if ((today.getMonth() - 1) >= dob.getMonth()) {
-                    return (today.getMonth() - 1) - dob.getMonth();
-                } else {
-                    return ((today.getMonth() - 1) + 12) - dob.getMonth();
-                }
-            }
-        } else {
-            if (today.getDate() >= dob.getDate()) {
-                return (today.getMonth() + 12) - dob.getMonth();
-            } else {
-                return ((today.getMonth() - 1) + 12) - dob.getMonth();
-            }
-        }
-    })();
-
-    year = (function() {
-        if (dob.getMonth() === today.getMonth()) {
-            if (dob.getDate() > today.getDate()) {
-                return (today.getFullYear() - 1) - dob.getFullYear();
-            } else {
-                return today.getFullYear() - dob.getFullYear();
-            }
-        } else {
-            if (dob.getMonth() > today.getMonth()) {
-                return (today.getFullYear() - 1) - dob.getFullYear();
-            } else {
-                return today.getFullYear() - dob.getFullYear();
-            }
-        }
-    });
-
-    age.innerHTML = "You are  <br>" + year + "Year" + "Months" + day + "Days,";
-
-    calTime = setInterval(samay, 1000);
+        // Display the result
+        document.getElementById('result').innerText = 'Your age is: ' + age + ' years.';
+    } else {
+        alert('Please select a birthdate.');
+    }
 }
-
-calcBtn.onclick = calculate; // Corrected function name
-
-function reset() {
-    age.innerHTML = "";
-    time.innerHTML = "";
-    clearInterval(calTime);
-    disBlock.style.display = 'none';
-}
-
-resetBtn.onclick = reset;
